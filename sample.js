9af87489da1a2241752f9a0fd852289bcf284684
Shailesh <shaileshf12@gmail.com>
2022-03-30 22:19:43 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,17 @@
+
+setInterval(()=>{
+    
+    date = new Date();
+    h = date.getHours();
+    m = date.getMinutes();
+    s = date.getSeconds();
+
+    hrotation = 30*h + m/2;
+    mrotation = 6*m;
+    srotation = 6*s;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+}, 1000)
